2b3ce965215ffc1c64edf0ee294da51b43ceb73c
martin <martin@hop.kcl.ac.uk>
2011-07-20 07:35:19 +0100

    fgets has internal buffering and screws up select

--- 1.c
+++ 1.c
@@ -12,31 +12,26 @@ max(int a,int b)
   return b;
 }
 
+char s[200];
+
 int
 main()
 {
-  FILE*af=fopen("2-to-1","r");
-  FILE*bf=fopen("3-to-1","r");
-  
-  int a=fileno(af);
-  int b=fileno(bf);
-  
+  int a=open("2-to-1",O_RDONLY);
+  int b=open("3-to-1",O_RDONLY);
 
-  char s[200];
+
   int count;
   for(count=0;count<10;count++){
-    fd_set rfds,efds;
+    fd_set rfds;
     FD_ZERO(&rfds);
     FD_SET(a,&rfds);
     FD_SET(b,&rfds);
-    FD_ZERO(&efds);
-    FD_SET(a,&efds);
-    FD_SET(b,&efds);
     
-    int r=select(max(a,b)+1,&rfds,NULL,&efds,NULL);
+    int r=select(max(a,b)+1,&rfds,NULL,NULL,NULL);
     
-    printf("%d select returned\n",count);
-    fflush(stdout);
+    //    printf("%d select returned\n",count);
+    //    fflush(stdout);
     if(r==-1 && errno==EINTR){
       printf("eintr\n");
       continue;
@@ -48,26 +43,11 @@ main()
     if(r){
       if(FD_ISSET(a,&rfds)){
 	printf("fd a\n");
-	if(0==fgets(s,200,af)){
-	  printf("fgets a returned 0\n");
-	  continue;
-	}
-	printf("1 %s\n",s);
-	fflush(stdout);
+	read(a, s, 200);
       }
-      if(FD_ISSET(a,&efds))
-	break;
-      if(FD_ISSET(b,&efds))
-	break;
-      
       if(FD_ISSET(b,&rfds)){
 	printf("fd b\n");
-	if(0==fgets(s,200,bf)){
-	  printf("fgets b returned 0\n");
-	  continue;
-	}
-	printf("2 %s\n",s);
-	fflush(stdout);
+	read(b, s, 200);
       }
     }
   }
